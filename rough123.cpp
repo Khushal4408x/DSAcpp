01d81f0e3e3f4ba0ba6de24acaacf222a50dcfae
Khushal4408x <yadavkhushal25@gmail.com>
2025-01-25 09:29:41 +0530

    Create rough123.cpp

---
+++ rough123.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int diagsum(int a,int mat[][3]){
+    for(int i=0,i<3,i++){
+        for(int j=0;j<3;j++){
+            if(i==j)
+        }
+    }
+    
+}
+int main() {
+    int mat[3][3]={{1,2,3},{4,5,6},{7,8,9}}
+    
+    
+    return 0;
+}
