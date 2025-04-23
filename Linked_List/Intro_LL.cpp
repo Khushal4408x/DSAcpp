018fa1e2d58981f915fa555d7086bf91cb325d6c
Khushal4408x <yadavkhushal25@gmail.com>
2025-04-23 21:25:20 +0530

    .

--- Linked_List/Intro_LL.cpp
+++ Linked_List/Intro_LL.cpp
@@ -17,10 +17,44 @@ class List{
     head=tail=NULL;
 }
     void push_front(int val){
-        Node* newnode=new Node(val);
-        
-    }
+        Node* temp= new Node (val);
+
+        if(head==NULL){
+            head=tail=temp;
+            return;
+        }
+        else{
+          temp->next=head;
+          head=temp;
+        }
+     }
+     void push_back(int val){
+        Node* temp=new Node(val);
+        if(head==NULL){
+           head=tail=temp;
+        }
+        else{
+            tail->next=temp;
+            temp->next=NULL;tail=temp;
+        }
+     }
+     void display(){
+        Node* temp=head;
+        while (temp!=NULL){
+            cout<<temp->data<<"->";
+            temp=temp->next;
+            
+        }
+        cout<<"NULL"<<endl;
+        cout << "Tail value: " <<tail->data << endl;
+
+     }
+
 };
 int main(){
+    List l1;
+    l1.push_front(3);   l1.push_front(4);   l1.push_front(2);
+    l1.push_back(7);
+    l1.display();
 
 }
