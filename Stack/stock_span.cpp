d8f22927ede200d662f5e351c85b24c78859be1a
Khushal4408x <yadavkhushal25@gmail.com>
2025-05-27 12:29:23 +0530

    .

---
+++ Stack/stock_span.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std ;
+#include <stack>
+#include<vector>
+int main(){
+    vector<int>price={100,80,60,70,60,75,85};
+    vector<int>span(price.size(),0);
+    stack<int>s;
+    int prevhigh;
+    for(int i=0;i<price.size();i++){
+        while(s.size()>0&&(price[s.top()]<=price[i])) {
+            s.pop();
+        }
+        if(s.empty()){
+            span[i]=i+1;
+        }
+        else{
+            span[i]=(i-s.top());
+        }
+        s.push(i); }
+
+     for(auto val:span) { 
+        cout<<val<<", ";
+     }   }
